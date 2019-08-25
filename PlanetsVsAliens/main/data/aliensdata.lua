f16213b47a57fe008ed4e7dcb7ee0e2c86aab9b7
AmauryTavares <amaurytavares16@hotmail.com>
2019-08-25 17:50:12 -0300

    Sistema de pontuação

--- PlanetsVsAliens/main/data/aliensdata.lua
+++ PlanetsVsAliens/main/data/aliensdata.lua
@@ -5,33 +5,33 @@ return {
 			name = 'Alien 1',
 			attack = 100,
 			life = 100, 
-			speed = 50,
+			speed = 500,
 			cooldownAttack = 1,
 			starsDrop = 100,
 			fragmentsDrop = 50,
-			damageLife = 1
+			damageLife = 50
 		},
 		{
 			id = 2,
 			name = 'Alien 2',
 			attack = 100,
 			life = 100, 
-			speed = 50,
+			speed = 500,
 			cooldownAttack = 1,
 			starsDrop = 100,
 			fragmentsDrop = 50,
-			damageLife = 1
+			damageLife = 50
 		},
 		{
 			id = 3,
 			name = 'Alien 3',
 			attack = 100,
 			life = 100, 
-			speed = 50,
+			speed = 500,
 			cooldownAttack = 1,
 			starsDrop = 100,
 			fragmentsDrop = 50,
-			damageLife = 1
+			damageLife = 50
 		}
 	}
 }
