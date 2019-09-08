85424435a06292326b50ec01f11c4dcebcfc0eaa
AmauryTavares <amaurytavares16@hotmail.com>
2019-09-08 01:17:08 -0300

    Sistema de fusao de planetas e spawn de bosses

--- PlanetsVsAliens/main/Planet/planet9/planet9.go
+++ PlanetsVsAliens/main/Planet/planet9/planet9.go
@@ -24,7 +24,7 @@ embedded_components {
   position {
     x: -15.0
     y: -15.0
-    z: 0.0
+    z: 1.0
   }
   rotation {
     x: 0.0
@@ -88,6 +88,60 @@ embedded_components {
   position {
     x: 0.0
     y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "lb_level"
+  type: "label"
+  data: "size {\n"
+  "  x: 128.0\n"
+  "  y: 32.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "scale {\n"
+  "  x: 1.31\n"
+  "  y: 1.021\n"
+  "  z: 1.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "color {\n"
+  "  x: 0.0\n"
+  "  y: 0.5019608\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "outline {\n"
+  "  x: 0.101960786\n"
+  "  y: 0.3019608\n"
+  "  z: 0.101960786\n"
+  "  w: 1.0\n"
+  "}\n"
+  "shadow {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "leading: 1.0\n"
+  "tracking: 0.0\n"
+  "pivot: PIVOT_CENTER\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "line_break: false\n"
+  "text: \"Nivel 1\"\n"
+  "font: \"/builtins/fonts/system_font.font\"\n"
+  "material: \"/builtins/fonts/label.material\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 77.0
     z: 0.0
   }
   rotation {
--- PlanetsVsAliens/main/data/aliensdata.lua
+++ PlanetsVsAliens/main/data/aliensdata.lua
@@ -3,35 +3,35 @@ return {
 		{
 			id = 1,
 			name = 'Alien 1',
-			attack = 100,
-			life = 100, 
-			speed = 500,
+			attack = 30,
+			life = 30, 
+			speed = 60,
 			cooldownAttack = 1,
 			starsDrop = 100,
 			fragmentsDrop = 50,
-			damageLife = 50
+			damageLife = 5
 		},
 		{
 			id = 2,
 			name = 'Alien 2',
-			attack = 100,
-			life = 100, 
-			speed = 500,
+			attack = 30,
+			life = 30, 
+			speed = 60,
 			cooldownAttack = 1,
 			starsDrop = 100,
 			fragmentsDrop = 50,
-			damageLife = 50
+			damageLife = 5
 		},
 		{
 			id = 3,
 			name = 'Alien 3',
-			attack = 100,
-			life = 100, 
-			speed = 500,
+			attack = 30,
+			life = 30, 
+			speed = 60,
 			cooldownAttack = 1,
 			starsDrop = 100,
 			fragmentsDrop = 50,
-			damageLife = 50
+			damageLife = 5
 		}
 	}
 }
